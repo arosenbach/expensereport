fa6e17e3310fc39f47c4454890726878306861b2
Thomas Foulon <thomas.foulon@salesforce.com>
2022-09-13 09:41:21 +0200

    First refacto (Rami)

--- expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
+++ expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
@@ -26,24 +26,13 @@ public class ExpenseReport {
         System.out.println(employee.getFirstName() + " " + employee.getLastName());
 
         for (Expense expense : expenses) {
-            if (expense.type == ExpenseType.DINNER || expense.type == ExpenseType.BREAKFAST) {
-                mealExpenses += expense.amount;
-            }
+            mealExpenses = computeMealExpenses(mealExpenses, expense);
 
-            String expenseName = "";
-            switch (expense.type) {
-                case DINNER:
-                    expenseName = "Dinner";
-                    break;
-                case BREAKFAST:
-                    expenseName = "Breakfast";
-                    break;
-                case CAR_RENTAL:
-                    expenseName = "Car Rental";
-                    break;
-            }
+            String expenseName = computeExpenseName(expense);
 
-            String mealOverExpensesMarker = expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X" : " ";
+            String mealOverExpensesMarker = " ";
+            if (expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000)
+                mealOverExpensesMarker = "X";
 
             System.out.println(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker);
 
@@ -66,5 +55,28 @@ public class ExpenseReport {
                 System.out.println("====== ACCEPTED ======");
             }
         }
+    }
+
+    private static String computeExpenseName(Expense expense) {
+        String expenseName = "";
+        switch (expense.type) {
+            case DINNER:
+                expenseName = "Dinner";
+                break;
+            case BREAKFAST:
+                expenseName = "Breakfast";
+                break;
+            case CAR_RENTAL:
+                expenseName = "Car Rental";
+                break;
+        }
+        return expenseName;
+    }
+
+    private static int computeMealExpenses(int mealExpenses, Expense expense) {
+        if (expense.type == ExpenseType.DINNER || expense.type == ExpenseType.BREAKFAST) {
+            mealExpenses += expense.amount;
+        }
+        return mealExpenses;
     }
 }
