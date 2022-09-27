16804a91419b114c1ce2a2c1b1acf12282b4e8db
rami al daher noufal <rnoufal@salesforce.com>
2022-09-27 11:05:15 +0200

    remove chaining if elses

--- expensereport-java/src/main/java/com/nelkinda/training/CostCenter.java
+++ expensereport-java/src/main/java/com/nelkinda/training/CostCenter.java
@@ -3,25 +3,13 @@ package com.nelkinda.training;
 import java.util.Optional;
 
 public class CostCenter {
-    private ExpensePolicy expensePolicy;
+    private final ExpensePolicy expensePolicy;
 
     public CostCenter(final ExpensePolicy expensePolicy) {
         this.expensePolicy = expensePolicy;
     }
 
     public Optional<ExpenseReportStatus> computeReportStatus(int total) {
-        return Optional.ofNullable(this.expensePolicy).map(policy -> computeExpenseReportStatus(policy, total));
-    }
-
-    private ExpenseReportStatus computeExpenseReportStatus(ExpensePolicy policy, int total) {
-        if (policy.getMaxAmount() < total) {
-            if (policy.rejectIfOversMaxAmount()) {
-                return ExpenseReportStatus.REJECTED;
-            } else {
-                return ExpenseReportStatus.ACCEPTED;
-            }
-        } else {
-            return ExpenseReportStatus.ACCEPTED;
-        }
+        return Optional.ofNullable(this.expensePolicy).map(policy -> policy.computeExpenseReportStatus(total));
     }
 }
--- expensereport-java/src/main/java/com/nelkinda/training/ExpensePolicy.java
+++ expensereport-java/src/main/java/com/nelkinda/training/ExpensePolicy.java
@@ -4,19 +4,17 @@ public class ExpensePolicy {
     private int maxAmount = 0;
     private boolean rejectIfOversMaxAmount = false;
 
-    public int getMaxAmount() {
-        return maxAmount;
-    }
-
-    public boolean rejectIfOversMaxAmount() {
-        return rejectIfOversMaxAmount;
-    }
-
-    public void setMaxAmount(int maxAmount){
+    public void setMaxAmount(int maxAmount) {
         this.maxAmount = maxAmount;
     }
 
     public void setRejectIfOversMaxAmount(final boolean rejectIfOversMaxAmount) {
         this.rejectIfOversMaxAmount = rejectIfOversMaxAmount;
+    }
+
+    public ExpenseReportStatus computeExpenseReportStatus(int total) {
+        return maxAmount < total && rejectIfOversMaxAmount ?
+                ExpenseReportStatus.REJECTED :
+                ExpenseReportStatus.ACCEPTED;
     }
 }
